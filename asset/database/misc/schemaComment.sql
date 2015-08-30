178dc89c8f1cbab2466cc66cb9d088545ba0fee5
Dave Anderson (xTuple) <danderson@xtuple.com>
2015-08-31 08:10:47 +1200

    Fix license message

--- asset/database/misc/schemaComment.sql
+++ asset/database/misc/schemaComment.sql
@@ -1,3 +1,3 @@
-COMMENT ON SCHEMA asset IS '  This file is part of the Fixed Asset Package for xTuple ERP, and is Copyright (c) 2012 by Anderson.NET New Zealand.  It is licensed to you under the CPAL.   This Package is not free software.  By using this software, you agree to be bound by the terms of the EULA.';
+COMMENT ON SCHEMA asset IS '  This file is part of the Fixed Asset Package for xTuple ERP, and is Copyright (c) 2012-2015 by Pentuple Ltd. New Zealand.  It is licensed to you under the CPAL. By using this software, you agree to be bound by the terms of the EULA.';
 
 GRANT ALL ON SCHEMA asset TO xtrole;
