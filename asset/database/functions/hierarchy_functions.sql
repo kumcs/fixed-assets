8d8b42d00f100695cd7ede9bff99572f1ffc2802
Dave Anderson (xTuple) <danderson@xtuple.com>
2018-04-26 08:54:19 +1200

    Asset List hierarchy version 2 Replace old methodology with a much simpler recursive query. Remove deprecated types/functions and update metaSQL to use the query directly

--- asset/database/functions/hierarchy_functions.sql
+++ asset/database/functions/hierarchy_functions.sql
@@ -1,85 +1,14 @@
 -- Fixed Asset Hierarchy functions
 -- Multi-level hierarchy display
--- Copyright (c) 2013 Pentuple Consulting Ltd. (New Zealand) dave@pentuple.co.nz
--- All Rights Reserved.
+-- Copyright (c) 2013 Pentuple Consulting Ltd. (New Zealand)
+--
+-- Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple. 
+-- See www.xtuple.com/EULA for the full text of the software license.
 
--- NOTE:  These functions must be run in this order hence the single file
-CREATE OR REPLACE FUNCTION asset.hierarchy_parent(asset integer)
-  RETURNS integer AS
-$BODY$
-DECLARE
-   tmpid integer;
-   parentid integer := 99;
-
-BEGIN
-
---  Determine the Top level parent id for sorting purposes
-	tmpid = asset;
-	
-	WHILE parentid != 0
-	LOOP
-	  SELECT id,COALESCE(asset_parentid, 0) 
-	  INTO tmpid, parentid
-	  FROM asset.asset
-	  WHERE id=tmpid;
-
-	  IF parentid != 0 THEN
-	    tmpid = parentid;
-	  END IF;  
-	END LOOP;
-
-	return tmpid;
-    
-END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION asset.hierarchy_parent(integer)
-  OWNER TO xtrole;
-
-CREATE OR REPLACE VIEW asset.t_hierarchy AS 
- SELECT asset.hierarchy_parent(asset.id) AS toplevel, asset.id, asset.id AS assetid, COALESCE(asset.asset_parentid, 0) AS parent, asset.asset_code AS value
-   FROM asset.asset
-  ORDER BY asset.hierarchy_parent(asset.id), COALESCE(asset.asset_parentid, 0), asset.id;
-
-ALTER TABLE asset.t_hierarchy
-  OWNER TO xtrole;
-GRANT ALL ON TABLE asset.t_hierarchy TO xtrole;
-
+-- NOTE:  These functions have been deprecated by a recursive PostgreSQL query
+DROP VIEW IF EXISTS asset.t_hierarchy CASCADE;
 DROP TYPE IF EXISTS asset.tp_hierarchy CASCADE;
-
-CREATE TYPE asset.tp_hierarchy AS
-   (node asset.t_hierarchy,
-    level integer);
-ALTER TYPE asset.tp_hierarchy
-  OWNER TO xtrole;
-
-CREATE OR REPLACE FUNCTION asset.fn_hierarchy_connect_by(integer, integer)
-  RETURNS SETOF asset.tp_hierarchy AS
-$BODY$
-        SELECT  CASE
-                WHEN node = 1 THEN
-                        (t_hierarchy, $2)::asset.tp_hierarchy
-                ELSE
-                        asset.fn_hierarchy_connect_by((q.t_hierarchy).id, $2 + 1)
-                END
-        FROM    (
-                SELECT  t_hierarchy, node
-                FROM    (
-                        SELECT  1 AS node
-                        UNION ALL
-                        SELECT  2
-                        ) nodes,
-                        asset.t_hierarchy
-                WHERE   parent = $1
-                ORDER BY
-                        id, node
-                ) q;
-$BODY$
-  LANGUAGE sql VOLATILE
-  COST 100
-  ROWS 1000;
-ALTER FUNCTION asset.fn_hierarchy_connect_by(integer, integer)
-  OWNER TO xtrole;
-
+DROP TYPE IF EXISTS asset.asset_list;
+DROP FUNCTION IF EXISTS asset.hierarchy_parent(integer);
+DROP FUNCTION IF EXISTS asset.fn_hierarchy_connect_by(integer, integer);
 
